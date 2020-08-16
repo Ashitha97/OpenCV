1d0be3a32d553e824234f1b0a6e9ecb97a464cc7
Ashitha A Nair <61631501+Ashitha97@users.noreply.github.com>
2020-08-16 20:05:22 +0530

    Add files via upload

---
+++ Read and write images.py
@@ -0,0 +1,10 @@
+import cv2
+img=cv2.imread('lena.jpg',-1)
+print(img)  #now we have just read the image
+cv2.imshow('image',img) #to see the image
+k=cv2.waitKey(0)
+if k==27:
+    cv2.destroyAllWindows()
+elif k==ord('s'):
+    cv2.imwrite('lena_copy.png',img)#to write images to other file
+    cv2.destroyAllWindows()
